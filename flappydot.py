bb44994444ca214f26b463a02bcc453f10b60a26
HuskyIsHere <72660373+HuskyIsHere@users.noreply.github.com>
2021-03-11 10:49:04 +0700

    Player jump and fall with gravity Resolve #1

--- flappydot.py
+++ flappydot.py
@@ -7,9 +7,16 @@ CANVAS_HEIGHT = 500
 
 UPDATE_DELAY = 33
 GRAVITY = 2.5
+STARTING_VELOCITY = -30
 
 class Dot(Sprite):
-    pass
+    def init_element(self):
+        self.vy = STARTING_VELOCITY
+
+    def update(self):
+        self.y += self.vy
+        self.vy += GRAVITY
+
 
 
 class FlappyGame(GameApp):
@@ -30,10 +37,9 @@ class FlappyGame(GameApp):
     def on_key_pressed(self, event):
         pass
 
-
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("Monkey Banana Game")
+    root.title("Flappy-dot Game")
  
     # do not allow window resizing
     root.resizable(False, False)
